25f2556894d578cbac60f22a40c6ff50ea70713b
Khumendra Sinha <khumendrasinha78@gmail.com>
2021-05-17 16:21:27 +0530

    NO MSG

--- pyshop/products/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'products'
-
-urlpatterns = [
-    path('', views.index, name='home'),
-    path('products/', views.index, name='home'),
-]
